1c632028d09f860d49947d48d64f770fdecdcb2f
abarne <celius377@gmail.com>
2019-08-20 14:56:39 -0500

    callbacks finished

--- assignments/closure.js
+++ assignments/closure.js
@@ -18,18 +18,28 @@ function aboutMe() {
 }
 
 aboutMe();
-//console.log('testing');
+
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 // ==== Challenge 2: Implement a "counter maker" function ====
-//const counterMaker = () => {
-// IMPLEMENTATION OF counterMaker:
-// 1- Declare a `count` variable with a value of 0. We will be mutating it, so declare it using `let`!
-// 2- Declare a function `counter`. It should increment and return `count`.
-//      NOTE: This `counter` function, being nested inside `counterMaker`,
-//      "closes over" the `count` variable. It can "see" it in the parent scope!
-// 3- Return the `counter` function.
-//};
+const counterMaker = () => {
+	// IMPLEMENTATION OF counterMaker:
+	// 1- Declare a `count` variable with a value of 0. We will be mutating it, so declare it using `let`!
+	// 2- Declare a function `counter`. It should increment and return `count`.
+	//      NOTE: This `counter` function, being nested inside `counterMaker`,
+	//      "closes over" the `count` variable. It can "see" it in the parent scope!
+	// 3- Return the `counter` function.
+
+	let count = 0;
+	function counter() {
+		if (count <= 10) {
+			count++;
+		} else {
+			count = 1;
+		}
+	}
+	return counter();
+};
 // Example usage: const myCounter = counterMaker();
 // myCounter(); // 1
 // myCounter(); // 2
@@ -39,8 +49,8 @@ aboutMe();
 // will refuse to go over the limit, and start back at 1.
 
 // ==== Challenge 4: Create a counter function with an object that can increment and decrement ====
-//const counterFactory = () => {
-// Return an object that has two methods called `increment` and `decrement`.
-// `increment` should increment a counter variable in closure scope and return it.
-// `decrement` should decrement the counter variable and return it.
-//};
+const counterFactory = () => {
+	// Return an object that has two methods called `increment` and `decrement`.
+	// `increment` should increment a counter variable in closure scope and return it.
+	// `decrement` should decrement the counter variable and return it.
+};
